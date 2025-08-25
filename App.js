908f1e9574778eef7f439f65f730a301921c82d2
shivamrishi2002 <shivam8005260520@gmail.com>
2025-08-25 10:55:22 +0530

    Create App.js

---
+++ App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import HomeScreen from "./screens/HomeScreen";
+import GroupScreen from "./screens/GroupScreen";
+import PersonScreen from "./screens/PersonScreen";
+import PaymentScreen from "./screens/PaymentScreen";
+
+const Stack = createStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Group" component={GroupScreen} />
+        <Stack.Screen name="Person" component={PersonScreen} />
+        <Stack.Screen name="Payment" component={PaymentScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+    }
